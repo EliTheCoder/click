d342691cbc30c79bad6c661fa256aa39f775316b
elithecoder <ex.games.org@gmail.com>
2018-10-09 17:43:57 -0500

    stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*
 
-Click
-Copyright (C) 2018 Eli Frigo
+The Alchemist's Clicker
+Copyright (C) 2018 Eli Frigo & Evan Mader
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
